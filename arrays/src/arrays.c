0d90abe8e9402616b64f1c49a5dba0eb9dcf221b
acpppf <acpppf@mail.missouri.edu>
2016-01-22 17:25:49 -0600

    Updated Array.c

--- arrays/src/arrays.c
+++ arrays/src/arrays.c
@@ -7,7 +7,7 @@
 // LOOK INTO MEMCPY, MEMCMP, FREAD, and FWRITE
 
 bool array_copy(const void *src, void *dst, const size_t elem_size, const size_t elem_count) {
-	if(!src || !dst || elem_size < 0 || elem_count < 0) {
+	if(!src || !dst || elem_size == 0 || elem_count == 0) {
 		return false;
 	}
 	memcpy(dst, src, elem_size * elem_count);
@@ -15,16 +15,17 @@ bool array_copy(const void *src, void *dst, const size_t elem_size, const size_t
 }
 
 bool array_is_equal(const void *data_one, void *data_two, const size_t elem_size, const size_t elem_count) {
-	if(!data_one || !data_two || elem_size < 0 || elem_count < 0) {	
+	if(!data_one || !data_two || elem_size == 0 || elem_count == 0) {	
 		return false;
 	}
-	if(memcmp(data_one, data_two, elem_size * elem_count)) {
+	if(memcmp(data_one, data_two, elem_size * elem_count) == 0) {
 		return true;
 	}
 	else {
 		return false;
 	}
 }
+
 ssize_t array_locate(const void *data, const void *target, const size_t elem_size, const size_t elem_count) { 
 
 	return 0;
