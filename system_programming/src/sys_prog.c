a0766ee8fe15c0c2904d180b8f90874de8873a5d
acpppf <acpppf@mail.missouri.edu>
2016-01-24 16:46:21 -0600

    Almost finished System_Programming

--- system_programming/src/sys_prog.c
+++ system_programming/src/sys_prog.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <byteswap.h>
 
 #include "../include/sys_prog.h"
 
@@ -43,13 +44,35 @@ bool bulk_write(const void *src, const char *output_filename, const size_t offse
         }
 }
 
-
+// Returns the file metadata given a filename   
+// \param query_filename the filename that will be queried for stats
+// \param metadata the buffer that contains the metadata of the queried filename
+// return true if operation was successful, else false
 bool file_stat(const char *query_filename, struct stat *metadata) {
-
-	return false;
+	if(!query_filename || !metadata) {
+		return false;
+	}
+	int file_desc = open(query_filename, O_RDONLY);
+	if(file_desc <= 0) {
+		close(file_desc);
+		return false;
+	} else {
+		fstat(file_desc, metadata);
+		close(file_desc);
+		return true;
+	}	
 }
 
+// Converts the endianess of the source data contents before storing into the dst_data.
+// The passed source data bits are swapped from little to big endian and vice versa. 
+// \param src_data the source data that contains content to be stored into the destination
+// \param dst_data the destination that stores src data
+// \param src_count the number of src_data elements
+// \return true if successful, else false for failure
 bool endianess_converter(uint32_t *src_data, uint32_t *dst_data, const size_t src_count) {
-
-	return false;
+	if(!src_data || !dst_data || src_count == 0) {
+		return false;
+	}
+	*dst_data = __bswap_32(*src_data);
+	return true;
 }
