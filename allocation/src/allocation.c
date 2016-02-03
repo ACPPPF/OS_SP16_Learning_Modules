72b331c1d89d0bd0162252633dbe400a7c250ae2
ACPPPF <acpppf@mail.missouri.edu>
2016-02-03 15:27:17 -0600

    Finished allocation

--- allocation/src/allocation.c
+++ allocation/src/allocation.c
@@ -1,12 +1,19 @@
 #include "../include/allocation.h"
+#include <stdint.h>
 
 void* allocate_array(size_t member_size, size_t nmember,bool clear) {
-	if(member_size == 0 || nmember == 0) {
+	if((signed int)member_size <= 0 || (signed int)nmember <= 0) {
+		return NULL;
+	}
+	if((short)member_size < 0 || (short)nmember < 0) {
 		return NULL;
 	}
 	void* array = NULL;
 	if(clear == 1) {
 		array = calloc(nmember, member_size);
+		if(array == NULL) {
+			return NULL;
+		}
 	} else if(clear == 0) {
 		array = malloc(nmember * member_size);
 		if(array == NULL) {
@@ -16,44 +23,49 @@ void* allocate_array(size_t member_size, size_t nmember,bool clear) {
 	return array;
 }
 
-/// Realloc
-/// - Does not initialize expanded memory
-/// - Original contents remain unchanged
-/// - If passed a null pointer then a malloc will occur.
-/// - May move memory to a new location
-
-/// Simple wrapper around realloc.
-/// \param void* - pointer to memory to resize.
-/// \param size_t - size of memory to allocate
-/// \return void* - pointer to reallocated memory region, may be same as original pointer.
 void* reallocate_array(void* ptr, size_t size) {
-	if(size == 0) {
+	if((signed int)size <= 0) {
 		return NULL;
 	} else if(!ptr) {
 		ptr = malloc(size);
-		return ptr;
 	} else {
-		return realloc(ptr, size);
+		ptr = realloc(ptr, size);
 	}
+	if(ptr == NULL) {
+		return NULL;
+	}
+	return ptr;
 }
 
 void deallocate_array(void** ptr) {
 	if(!ptr) {
 		return;
 	}
-	void** temp = ptr;
-	int counter = 0;
-	while(temp[counter + 1]) {
-		counter++;
-	}
-	for(int index = 0; index < counter; ++index) {
-		if(ptr[index] != NULL) {
-			free(ptr[index]);
-			ptr[index] = NULL;
-		}
-	}
+	free(*ptr);
+	*ptr = NULL;
 }
 
+/// Takes a file name and reads a line into a newly allocated buffer
+/// \param char* - filename to read from
+/// \return char* - Pointer to malloced heap space containing buffer
 char* read_line_to_buffer(char* filename) {
-	return NULL;
+	if(!filename) {
+		return NULL;
+	}
+	FILE *fp;
+	size_t size;
+	char* buffer;
+	
+	fp = fopen(filename, "rb");
+	fseek(fp, 0, SEEK_END);
+	size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	if(fp == NULL) {
+		return NULL;
+	}
+	buffer = (char *)malloc(size + 1);
+	fread(buffer, sizeof(char), size, fp);
+	fclose(fp);
+	buffer[size] = 0;
+	return buffer;
 }
